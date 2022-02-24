b5929498c11cefa00d5861ed378758636cf19b31
alexanderbozhko <aleksandrvikbozhko@gmail.com>
2022-02-24 22:56:44 +0400

    откорректировал класс ArrayAfterFour

---
+++ src/main/java/com/tests/ArrayAfterFour.java
@@ -0,0 +1,39 @@
+package com.tests;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+import java.util.Arrays;
+
+
+public class ArrayAfterFour {
+	private static final Logger logger = LogManager.getLogger(ArrayAfterFour.class.getName());
+
+	public static int[] getArrayAfterFour(int... arg) {
+
+		try {
+			int lastIndex = -1;
+			for (int i = arg.length - 1; i >= 0; i--) {
+				if (arg[i] == 4) {
+					lastIndex = i + 1;
+					return Arrays.copyOfRange(arg, lastIndex, arg.length);
+				}
+			}
+			throw new RuntimeException();
+		} catch (RuntimeException e) {
+			logger.info(" в массиве нет значения 4");
+			logger.info(e.getMessage(), e);
+			e.printStackTrace();
+		}
+		return arg;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {1, 5, 6, 0, 4, 11, 2, 3, 7};
+		int[] arr1 = {1, 5, 6, 0, 7, 11, 2, 3, 7};
+		int[] arr2 = {1, 5, 6, 0, 4, 11, 4, 3, 7};
+		System.out.println(java.util.Arrays.toString(getArrayAfterFour(arr2)));
+
+	}
+}
--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -1,19 +1,16 @@
-import com.tests.Array;
+import com.tests.ArrayAfterFour;
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
 import java.util.Arrays;
 
 public class ArrayTest {
-	Array array;
+	ArrayAfterFour array;
 
 	@Before
 	public int[] init() {
-		array = new Array();
+		array = new ArrayAfterFour();
 		int sizeArr = (int) (Math.random() * 10);
 		int[] arr = new int[sizeArr];
 		for (int i = 0; i < sizeArr; i++) {
@@ -27,7 +24,10 @@ public class ArrayTest {
 	@Test
 	public void getArrayAfterFourTest(int[] arr) {
 		arr = new int[]{1, 5, 6, 0, 4, 11, 2, 3, 7};
-			Assertions.assertTrue(4, Array.getArrayAfterFour(arr));
+		for (int e:
+		     arr) {
+			Assertions.assertTrue(e == 4, String.valueOf(ArrayAfterFour.getArrayAfterFour(arr)));
+		}
 
 	}
 
